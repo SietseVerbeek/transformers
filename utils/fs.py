de3ccba265e36130519ce3e9f446a7a343cb0b93
Sietse Verbeek <sietse.verbeek@outlook.com>
2024-12-03 16:20:49 +0100

    Change checkpoint loss to complete logs

--- utils/fs.py
+++ utils/fs.py
@@ -14,24 +14,27 @@ def results_dir(filename: str) -> str:
 
 
 def save_checkpoint(
-    model: nn.Module, optimizer: Optimizer, loss_log: list, epoch: int, filename: str
+    model: nn.Module, optimizer: Optimizer, logs: dict[str, list], epoch: int, filename: str
 ) -> None:
     state = {
         "epoch": epoch,
         "state_dict": model.state_dict(),
         "optimizer": optimizer.state_dict(),
-        "loss_log": loss_log,
+        "logs": logs,
     }
     torch.save(state, filename)
 
 
 # https://discuss.pytorch.org/t/loading-a-saved-model-for-continue-training/17244/3
 def load_checkpoint(
-    model: nn.Module, optimizer: Optimizer, filename: str
-) -> Tuple[nn.Module, Optimizer, int, list]:
+    model: nn.Module, optimizer: Optimizer | None, filename: str
+) -> Tuple[nn.Module, Optimizer, int, dict[str, list]]:
 
     start_epoch = 0
-    loss_log = []
+    logs = {
+        "loss": [],
+        "logs": []
+    }
 
     if os.path.isfile(filename):
 
@@ -40,8 +43,10 @@ def load_checkpoint(
 
         start_epoch = checkpoint["epoch"] + 1
         model.load_state_dict(checkpoint["state_dict"])
-        optimizer.load_state_dict(checkpoint["optimizer"])
-        loss_log = checkpoint["loss_log"]
+        logs = checkpoint["logs"]
+
+        if optimizer:
+            optimizer.load_state_dict(checkpoint["optimizer"])
 
         print(
             "=> loaded checkpoint '{}' (epoch {})".format(filename, checkpoint["epoch"])
@@ -50,4 +55,4 @@ def load_checkpoint(
     else:
         print("=> no checkpoint found at '{}'".format(filename))
 
-    return model, optimizer, start_epoch, loss_log
+    return model, optimizer, start_epoch, logs
