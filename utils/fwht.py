564ce19d8d0807e38b3cbb3d12ff2560aa1bd54e
Sietse Verbeek <sietse.verbeek@outlook.com>
2025-03-27 20:27:09 +0100

    Spin model with strong pairwise interaction in groups

---
+++ utils/fwht.py
@@ -0,0 +1,178 @@
+import numpy.typing as npt
+import numpy as np
+from numba import njit
+import torch
+
+
+@njit(fastmath=True)
+def fwht(u):
+    v = u.copy()
+    h = 1
+    lv = len(v)
+    while h < lv:
+        for i in range(0, lv, h * 2):
+            for j in range(i, i + h):
+                x = v[j]
+                y = v[j + h]
+                v[j] = x + y
+                v[j + h] = x - y
+        h *= 2
+    return v
+
+
+def generate_data(model, pars, n, states, N=100000):
+    """
+    model:   list/array of integer values corresponding to interactions
+    pars:    list/array of real valued parameters of same length as model
+    n:       number of variables
+    states:  list/array of all 2**n states in desired format
+    N:       number of data samples to return
+    """
+
+    if len(model) != len(pars):
+        raise ValueError("number of parameters should match number of interactions")
+
+    g = np.zeros(2**n)
+    g[model] = pars
+    u = np.exp(fwht(g))
+    p = u / np.sum(u)
+    data = np.random.choice(states, N, p=p)
+
+    return data
+
+
+def int_to_binary_array(configurations: npt.NDArray, N: int) -> npt.NDArray[np.uint8]:
+    """
+    Converts numpy array of integers into a 2d array where the second axis
+    is the binary representation of the integers with width N
+
+    Arguments:
+        configurations - numpy array of integers
+        N   - width of binary representation (amount of sites of MCM)
+
+    Output:
+        binary - binary representations of integers
+            (len(configurations), N) numpy array
+    """
+
+    binary = np.zeros((configurations.shape[0], N), dtype=np.uint8)
+
+    for i in range(N):
+        binary[:, N - i - 1] = (configurations >> i) & 1
+
+    return binary
+
+
+def pairwise_interactions(groupings):
+    """
+    Creates integer representations of pairwise interactions between members of a group
+    defined in the groupings variable
+
+    Arguments:
+        groupings: boolean array
+
+    Output:
+        interactions: integer array
+    """
+
+    interactions = []
+    N_sites = groupings.shape[-1]
+
+    for group in groupings:
+        indices = np.where(group)[0]  # Get active variable indices
+
+        for i in range(len(indices) - 1):  # Iterate over neighbor pairs
+            print(N_sites - 1 - indices[i])
+            pair_mask = (1 << N_sites - 1 - indices[i]) | (
+                1 << N_sites - 1 - indices[i + 1]
+            )  # Create bitmask using bit shifts
+            interactions.append(pair_mask)
+
+    return np.array(interactions)
+
+
+def gen_spin_model_batch(
+    weight: float,
+    batch_size: int,
+    set_size: int,
+    groupings: npt.NDArray[np.bool_],
+    device="cuda",
+):
+    N_sites = groupings.shape[-1]
+    N_maps = groupings.shape[0]
+    configs_per_map = batch_size // N_maps
+    batch_size = configs_per_map * N_maps
+
+    tgt = torch.tensor(
+        np.repeat(groupings, configs_per_map, axis=0), dtype=torch.long, device=device
+    )
+    src = torch.empty((batch_size, set_size, N_sites), dtype=torch.long, device=device)
+
+    model = np.arange(2**N_sites)
+    states = int_to_binary_array(model, N_sites)
+
+    for idx in range(N_maps):
+        weights = np.zeros(2**N_sites)
+        print(groupings[idx])
+        interactions = pairwise_interactions(groupings[idx])
+        print(interactions)
+        for i in interactions:
+            print(bin(i))
+        weights[interactions] = weight
+
+        choices = generate_data(model, weights, N_sites, model, configs_per_map * set_size)
+        print(states[choices].shape)
+        src[idx * configs_per_map : (idx + 1) * configs_per_map] = torch.tensor(
+            states[choices].reshape(-1, set_size, N_sites)
+        )
+
+    return src, tgt
+
+if __name__ == "__main__":
+    # n = 6
+    # model = np.arange(2**n)
+    # states = int_to_binary_array(model, n)
+    # g = np.zeros_like(model)
+
+    # for i in range(n - 1):
+    #     if i != 3:
+    #         g[3 << i] = 3
+
+    # choices = generate_data(model, g, n, model)
+    # out = states[choices]
+    # print(np.unique(out, axis=0, return_counts=True))
+    # for i in np.unique(out):
+    #     print(bin(i))
+
+    # Example input
+    groups = np.array(
+        [
+            [
+                [True, False, False, False, False, False],
+                [False, True, True, True, True, True],
+            ],
+            [
+                [True, True, False, False, False, False],
+                [False, False, True, True, True, True],
+            ],
+            [
+                [True, True, True, False, False, False],
+                [False, False, False, True, True, True],
+            ],
+            [
+                [True, True, True, True, False, False],
+                [False, False, False, False, True, True],
+            ],
+            [
+                [True, True, True, True, True, False],
+                [False, False, False, False, False, True],
+            ],
+            [
+                [True, True, True, True, True, True],
+                [False, False, False, False, False, False],
+            ],
+        ]
+    )
+
+    print(gen_spin_model_batch(1.5, 12, 5, groups))
+    # print(pairwise_interactions(groups))
