5e9d2b3bc429c253717f3419ca82bd7f4a4ed28f
Sietse Verbeek <sietse.verbeek@outlook.com>
2025-06-10 16:42:14 +0200

    Add mean voi plot per border pos script

---
+++ mean_voi_per_border.py
@@ -0,0 +1,121 @@
+import argparse
+import math
+import matplotlib.pyplot as plt
+from numba import njit
+import numpy as np
+import os
+
+import torch
+
+from models.PMATransformer import PMA_GroupingModel
+from pma import Config, _results_dir
+from utils.fs import load_checkpoint
+from utils.fwht import gen_spin_model_batch
+from utils.masking import create_mask
+from utils.tests import normalized_vi
+
+@njit
+def normalized_vi_loop(arr1, arr2):
+    num_clusterings = arr1.shape[0]
+    output = np.zeros(num_clusterings, dtype=np.float64)
+
+    for i in range(num_clusterings):
+        output[i] = normalized_vi(arr1[i], arr2[i])
+
+    return output
+
+
+def torch_check(input):
+    if isinstance(input, torch.Tensor):
+        if input.is_cuda:
+            return input.cpu().numpy()
+        return input.numpy()
+    return input
+
+
+def batch_normalized_vi(results, truth):
+    results = torch_check(results)
+    truth = torch_check(truth)
+
+    return normalized_vi_loop(results, truth)
+
+if __name__ == "__main__":
+
+
+    N = 10
+    device = "cuda"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", type=str)
+
+    args = parser.parse_args()
+
+    c = Config(args.config)
+    model_file = _results_dir(f"model_{c.model_id}.params")
+    checkpoint_file = _results_dir(f"model_{c.model_id}__id_{c.train_id}.pth")
+
+    if os.path.isfile(model_file):
+        model = PMA_GroupingModel.from_file(model_file).to(device)
+    else:
+        raise FileNotFoundError(f"model file with id {model_file} not found")
+
+    model, _, _, logs = load_checkpoint(checkpoint_file, model, None)
+
+    model.eval()
+
+    def labelings_one_border(length):
+        return np.array([[0] * (length - i) + [1] * i for i in range(length)])
+
+    groupings = labelings_one_border(N)
+
+    betas = np.linspace(.1, 1, 20, endpoint=True)
+
+    n = len(groupings)
+    cols = 3  # Choose the number of columns (you can tweak this)
+    rows = math.ceil(n / cols)
+
+    # Create subplots
+    fig, axes = plt.subplots(rows, cols, figsize=(cols * 4, rows * 3))
+    axes = axes.flatten()  # Flatten in case of 2D array of axes
+    
+    for i, grouping in enumerate(groupings):
+        means = np.empty_like(betas)
+        stds = np.empty_like(betas)
+        ax = axes[i]
+
+        for j, beta in enumerate(betas): 
+            src, tgt = gen_spin_model_batch(beta, 10, 50, grouping[None, ...])
+
+            batch_size = src.shape[0]
+            output = torch.zeros((batch_size, 1), dtype=torch.int64, device=device)
+
+            for _ in range(N -1):
+
+                src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(
+                    src, output, pad_idx=4, device=device
+                )
+
+                logits = model(
+                    src,
+                    output,
+                    tgt_mask=tgt_mask,
+                )
+
+                idx = logits[:, -1].argmax(dim=-1).view(-1, 1)
+                output = torch.cat((output, idx), dim=-1)
+
+            fraction = (output == tgt).count_nonzero() / output.nelement()
+            var_of_info = batch_normalized_vi(output, tgt)
+
+            means[j] = var_of_info.mean()
+            stds[j] = var_of_info.std()
+            # print('mean', var_of_info.mean())
+            # print('std', var_of_info.std())
+        ax.errorbar(betas, means, yerr=stds)
+        ax.set_title(f"border pos {N - i}")
+        # Hide any unused subplots
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])  # Or: axes[j].axis('off')
+
+    plt.tight_layout()
+    plt.savefig(f'results/pma/mean_voi_beta_border_pos__{c.model_id}__{c.train_id}')
+
